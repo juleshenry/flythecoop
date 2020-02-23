485e4df98f1bfe987c3b10d17a0bce56b9b4c0b4
juleshenry <julianplushenry@gmail.com>
2020-02-23 15:28:35 -0500

    nfollowers

--- brainmeld.py
+++ brainmeld.py
@@ -91,8 +91,6 @@ def weight(sFollowing, in_set): ##returns ordered list
         ret[x] = i
     return {k: v for k, v in sorted(ret.items(), key=lambda item: item[1])}
 
-
-
 def get_probes(node, probe_cnt):
     probes = []
     s = twint.Config()
@@ -112,32 +110,36 @@ def probe(source, target):
     for j in target_probes: print(j)
 
 class probe_twitter:
-    def __init__(self,source,target):
-        self.driver = webdriver.Chrome() #chrome 76.0.3809.126
-        self.driver.implicitly_wait(2)
-        self.get_followers(source)
-    
-    def get_followers(self, node):
+    def __init__(self, source, target):
+       
+        self.followers = []
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(options=chrome_options) #chrome 76.0.3809.126
+        self.driver.implicitly_wait(10)
+        self.followers = self.get_followers(source, 500)
+        print(self.followers)
+
+    def get_followers(self, node, nfollowers):
         self.driver.get('https://twitter.com/' + node + '/following')
         self.driver.find_element_by_css_selector('#react-root > div > div > div > header > div.css-1dbjc4n.r-yfoy6g.r-o4zss7.r-rull8r.r-qklmqi.r-1d2f490.r-1xcajam.r-zchlnj.r-ipm5af.r-1siec45.r-o7ynqc.r-axxi2z.r-136ojw6 > div.css-1dbjc4n.r-1jgb5lz.r-sb58tz.r-13qz1uu > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1h3ijdo.r-58zi21 > div.css-1dbjc4n.r-1awozwy.r-1pz39u2.r-18u37iz.r-16y2uox > div:nth-child(1) > a > div > span > span').click()
-        time.sleep(4)
         self.driver.find_element_by_css_selector("#react-root > div > div > div.css-1dbjc4n.r-1pi2tsx.r-13qz1uu.r-417010 > main > div > div > form > div > div:nth-child(6) > label > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1udh08x > div > input").click() # Click Login
-        time.sleep(4)
         input_user = self.driver.find_elements_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[1]/label/div[2]/div/input')[0]
         input_user.click()
         input_user.send_keys('julianplushenry@gmail.com') # send username
-        time.sleep(4)
         input_pass = self.driver.find_elements_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[2]/label/div[2]/div/input')[0]
         input_pass.click()
         input_pass.send_keys('passPasspazz') # send pass
-        time.sleep(4)
         self.driver.find_elements_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[3]/div/div/span/span')[0].click() # Do Login
-        time.sleep(10)
-        for i in range(1,16):
-            print(self.driver.find_element_by_css_selector('#react-root > div > div > div > main > div > div > div > div > div > div:nth-child(2) > section > div > div > div > div:nth-child('+ str(i) + ') > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-5f2r5o > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs > a > div > div.css-1dbjc4n.r-18u37iz.r-1wbh5a2 > div > span').text)
-
-    def sel_probe(source, target):
-        pass
+        followers = []
+        # self.driver.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
+        while (len(followers) != nfollowers):
+            pass
+        for i in range(1,nfollowers+1):
+            followers += [self.driver.find_element_by_css_selector('#react-root > div > div > div > main > div > div > div > div > div > div:nth-child(2) > section > div > div > div > div:nth-child('+ str(i) + ') > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-5f2r5o > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs > a > div > div.css-1dbjc4n.r-18u37iz.r-1wbh5a2 > div > span').text]
+        return followers
+    # def sel_probe(source, target):
+    #     pass
     #react-root > div > div > div > main > div > div > div > div > div > div:nth-child(2) > section > div > div > div > div:nth-child(3) > div > div > div > div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-5f2r5o > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs > a > div > div.css-1dbjc4n.r-18u37iz.r-1wbh5a2 > div > span
 if __name__ == '__main__':
     probe_twitter('BarackObama', 'KeltonMadden')
